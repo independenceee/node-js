3eedd0c3be0713eb729c2b4e760dd589b125abb7
Independence <nguyenkhanh17112003@gmail.com>
2023-03-26 20:16:59 +0700

    http protocol create app

--- 02_Express/src/01_http-basics.js
+++ 02_Express/src/01_http-basics.js
@@ -1,4 +1,29 @@
 const http = require("http");
 
 
-const server
+const server = http.createServer(function(request, response) {
+    const url = request.url;
+    console.log(url);
+
+    if(url === "/") {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        response.write("<h1>Home Page</h1>");
+        response.end();
+    } else if(url === "/about") {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        response.write("<h1>About Page</h1>");
+        response.end();
+    } else {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        response.write("<h1>Page not Found</h1>");
+        response.end();
+    }
+})
+
+server.listen(5000);
---
+++ 02_Express/src/index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+
+const server = http.createServer(function(request, response) {
+    const url = request.url;
+
+    if(url === "/") {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        response.write("<h1>Home Page</h1>");
+        response.end();
+    } else if(url === "/about") {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        response.write("<h1>About Page</h1>");
+        response.end();
+    } else {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        response.write("<h1>Page not Found</h1>");
+        response.end();
+    }
+})
+
+server.listen(5000);
